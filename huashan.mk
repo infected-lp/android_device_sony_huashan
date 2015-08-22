2f61697e00bbf6a539ab2c0a7f39cb4b7958af1f
AdrianDC <radian.dc@gmail.com>
2015-08-22 15:53:45 +0200

    huashan: Add Doze Sensors detection
    - Add gestures panel to the Settings
    
    - Features :
      - Hand wave Doze if device is not face down
      - Pick-up Doze if was face down or taken in hand
      - Pocket Doze if device unstowed vertically
      - Proximity Wake if device is not face down
    
    - Sensors :
      - Proximity sensor used for all the features
      - Accelerometer and Magnetometer used for the orientation
      - Accelerometer put in idle for the Pick-up with face up
        - This sensor will go in deep-sleep with the phone
    
    Change-Id: I33fdb04d2131acd284c76993a69fdb342c3a39b0
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

--- huashan.mk
+++ huashan.mk
@@ -272,6 +272,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.composition.type=c2d \
     persist.hwc.mdpcomp.enable=true
 
+# Doze mode
+PRODUCT_PACKAGES += \
+    SensorsDoze
+
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true
