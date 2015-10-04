b0f06eb9994f25abf79a54b288f08c0256f163ad
Adrian DC <AdrianDC@users.noreply.github.com>
2015-10-04 18:17:33 +0200

    Remove CM deps

--- doze/src/com/cyanogenmod/settings/device/TouchscreenGestureSettings.java
+++ doze/src/com/cyanogenmod/settings/device/TouchscreenGestureSettings.java
@@ -16,8 +16,6 @@
 
 package com.cyanogenmod.settings.device;
 
-import com.android.internal.util.cm.ScreenType;
-
 import android.app.ActionBar;
 import android.os.Bundle;
 import android.preference.Preference;
@@ -76,9 +74,7 @@ public class TouchscreenGestureSettings extends PreferenceActivity {
         super.onResume();
 
         // If running on a phone, remove padding around the listview
-        if (!ScreenType.isTablet(this)) {
-            getListView().setPadding(0, 0, 0, 0);
-        }
+        getListView().setPadding(0, 0, 0, 0);
     }
 
     @Override
